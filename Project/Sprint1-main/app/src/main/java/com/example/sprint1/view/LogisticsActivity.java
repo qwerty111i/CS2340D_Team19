2165a81f861837f9cfe1ae215d0526a19f230e9f
Adwaith Ramesh <39378690+qwerty111i@users.noreply.github.com>
2024-09-26 18:01:57 -0400

    Revert "Set up MVVM architecture"

--- Project/Sprint1-main/app/src/main/java/com/example/sprint1/view/LogisticsActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.sprint1.view;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class LogisticsActivity extends AppCompatActivity {
-}
--- Project/Sprint1-main/app/src/main/java/com/example/sprint1/view/MainActivity.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1/view/MainActivity.java
@@ -8,11 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.sprint1.BR;
 import com.example.sprint1.R;
-import com.example.sprint1.viewmodel.LoginViewModel;
-import com.example.sprint1.databinding.ActivityMainBinding;
-import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,20 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
-        //use databinding to inflate the layout
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
- //      setContentView(R.layout.activity_main);
-
-        //create viewmodel
-        LoginViewModel viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
-        //bind viewmodel to layout
-        binding.setVariable(BR.ViewModel, viewModel);
-        binding.setLifecycleOwner(this);
-
-
-
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
