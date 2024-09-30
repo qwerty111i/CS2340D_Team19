cbfb4e123ee95eb9ad58faea190a6b05aabed8b6
Adwaith Ramesh <adwaithsramesh@gmail.com>
2024-09-30 12:49:31 -0400

    Added Login/Sign-up, integrated with Firebase

--- Project/Sprint1-main/app/src/main/java/com/example/sprint1/model/User.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1/model/User.java
@@ -1,4 +1,17 @@
 package com.example.sprint1.model;
 
 public class User {
+    private String email;
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
