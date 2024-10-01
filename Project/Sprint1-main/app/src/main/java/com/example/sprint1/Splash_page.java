50e7573104ac7826aed6f1513b9b07689b294822
hailmig7 <sahadev.bharath@gmail.com>
2024-09-30 20:54:39 -0400

    updated splashscreen with slight progress

--- Project/Sprint1-main/app/src/main/java/com/example/sprint1/Splash_page.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1/Splash_page.java
@@ -1,12 +1,17 @@
 package com.example.sprint1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.sprint1.view.MainActivity;
 
 public class Splash_page extends AppCompatActivity {
 
@@ -15,10 +20,32 @@ public class Splash_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_page);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        Button quitButton = findViewById(R.id.quit_button);
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
+
+
+        Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Splash_page.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
