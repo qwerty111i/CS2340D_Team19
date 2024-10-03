63039643b82a25f1c17635278d3661f08e8bdf78
Bimikel <mzuo30@gatech.edu>
2024-10-03 17:03:17 -0400

    added navigation bar to all relevant screens (including the 6 main screens and home screen)

--- Project/Sprint1-main/app/src/main/java/com/example/sprint1/view/MainActivity.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1/view/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sprint1.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,7 @@ import com.example.sprint1.BR;
 import com.example.sprint1.R;
 import com.example.sprint1.viewmodel.LoginViewModel;
 import com.example.sprint1.databinding.ActivityMainBinding;
+
 import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,6 +42,61 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageButton btnLogistics = findViewById(R.id.btn_logistics);
+        ImageButton btnAccom = findViewById(R.id.btn_accom);
+        ImageButton btnDest = findViewById(R.id.btn_dest);
+        ImageButton btnDining = findViewById(R.id.btn_dining);
+        ImageButton btnTransport = findViewById(R.id.btn_transport);
+        ImageButton btnTravel = findViewById(R.id.btn_travel);
+
+        btnLogistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LogisticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnDest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DestinationsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnDining.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DiningActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnAccom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AccommodationsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnTravel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TravelActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnTransport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TransportationActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
