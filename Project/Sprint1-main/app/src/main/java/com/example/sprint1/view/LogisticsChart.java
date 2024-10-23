d13933ff3465d9545669e4e9dd92e000d4e42681
Bimikel <mzuo30@gatech.edu>
2024-10-23 13:34:55 -0400

    Extra checkstyle fix in LogisticsChart.java

--- Project/Sprint1-main/app/src/main/java/com/example/sprint1/view/LogisticsChart.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1/view/LogisticsChart.java
@@ -3,7 +3,6 @@ package com.example.sprint1.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
