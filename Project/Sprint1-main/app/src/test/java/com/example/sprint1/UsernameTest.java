62e5177a90ad195bf68cac5ae02ef32eb177ff19
Matthew <msebastian3304@gmail.com>
2024-10-27 15:24:45 -0400

    Add Username Test Cases

---
+++ Project/Sprint1-main/app/src/test/java/com/example/sprint1/UsernameTest.java
@@ -0,0 +1,52 @@
+package com.example.sprint1;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import com.example.sprint1.viewmodel.LoginViewModel;
+import com.example.sprint1.viewmodel.SignupViewModel;
+
+import org.robolectric.RobolectricTestRunner;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(RobolectricTestRunner.class)
+public class UsernameTest {
+    // Allows live data to execute synchronously
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    private SignupViewModel viewModel;
+
+    // Creating a new instance of LoginViewModel
+    @Before
+    public void setUp() {
+        Boolean test = true;
+        viewModel = new SignupViewModel(test);
+    }
+
+    // Matthew
+    @Test
+    public void validEmailUsernamePassword() {
+        viewModel.signInValidation();
+        viewModel.setEmail("ad@min.com");
+        viewModel.setUsername("admin");
+        viewModel.setPassword("123456");
+        viewModel.signInValidation();
+        assertNull(viewModel.getEmailError().getValue());
+        assertNull(viewModel.getUsernameError().getValue());
+        assertNull(viewModel.getPasswordError().getValue());
+
+        viewModel.signInValidation();
+        viewModel.setEmail("a d@min.com");
+        viewModel.setUsername("  ");
+        viewModel.setPassword("123456");
+        viewModel.signInValidation();
+        assertEquals("Cannot contain Whitespace!", viewModel.getEmailError().getValue());
+        assertEquals("Username cannot contain whitespace!", viewModel.getUsernameError().getValue());
+        assertNull(viewModel.getPasswordError().getValue());
+
+    }
+}
