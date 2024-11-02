fd762fd76aa0456ce2e9a31cb9bf84cfc9402c49
Matthew <msebastian3304@gmail.com>
2024-11-02 17:22:13 -0400

    Hide implicit public constructor

--- Code_Smells/src/EmailSender.java
+++ Code_Smells/src/EmailSender.java
@@ -1,5 +1,9 @@
 
 public class EmailSender {
+    // Make private constructor to hide the implicit public constructor
+    private EmailSender() {
+        throw new IllegalStateException("Utility class");
+    }
     public static void sendEmail(String customerEmail, String subject, String message){
         System.out.println("Email to: " + customerEmail);
         System.out.println("Subject: " + subject);
