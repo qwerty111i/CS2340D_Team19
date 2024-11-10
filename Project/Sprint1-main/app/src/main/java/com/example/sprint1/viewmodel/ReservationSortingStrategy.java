b5cec11f7d300c40f7a82852d75a235cb13761e6
James Gao <jamesg@gatech.edu>
2024-11-10 11:07:26 -0500

    add strategy files

--- Project/Sprint1-main/app/src/main/java/com/example/sprint1/viewmodel/ReservationSortingStrategy.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1/viewmodel/ReservationSortingStrategy.java
@@ -1,2 +1,8 @@
-package com.example.sprint1.viewmodel;public class ReservationSortingStrategy {
+package com.example.sprint1.viewmodel;
+
+import com.example.sprint1.model.ReservationDetails;
+import java.util.List;
+
+public interface ReservationSortingStrategy {
+    void sort(List<ReservationDetails> reservations);
 }
--- Project/Sprint1-main/app/src/main/java/com/example/sprint1/viewmodel/SortByStartDate.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1/viewmodel/SortByStartDate.java
@@ -1,10 +1,8 @@
 package com.example.sprint1.viewmodel;
 
 import com.example.sprint1.model.ReservationDetails;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -18,9 +16,9 @@ public class SortByStartDate implements ReservationSortingStrategy {
             try {
                 Date date1 = dateFormat.parse(r1.getStartDate());
                 Date date2 = dateFormat.parse(r2.getStartDate());
+                assert date1 != null;
                 return date1.compareTo(date2);
-            } catch (ParseException e) {
-                e.printStackTrace();
+            } catch (Exception e) {
                 return 0;
             }
         });
