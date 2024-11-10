195135dd192247b478e9626aa36f2870cbd4a263
James Gao <jamesg@gatech.edu>
2024-11-10 11:07:26 -0500

    add strategy files

--- Project/Sprint1-main/app/src/main/java/com/example/sprint1/viewmodel/ReservationSorter.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1/viewmodel/ReservationSorter.java
@@ -1,2 +1,18 @@
-package com.example.sprint1.viewmodel;public class ReservationSorter {
+package com.example.sprint1.viewmodel;
+
+import com.example.sprint1.model.ReservationDetails;
+import java.util.List;
+
+public class ReservationSorter {
+    private ReservationSortingStrategy strategy;
+
+    public void setStrategy(ReservationSortingStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void sortReservations(List<ReservationDetails> reservations) {
+        if (strategy != null) {
+            strategy.sort(reservations);
+        }
+    }
 }
