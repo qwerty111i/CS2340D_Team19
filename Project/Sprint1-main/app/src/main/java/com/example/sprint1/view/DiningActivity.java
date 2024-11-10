dbc8c77de472fa5be84009f44713ae3d790da58c
Adwaith Ramesh <adwaithsramesh@gmail.com>
2024-11-10 15:55:18 -0500

    Style sort options

--- Project/Sprint1-main/app/src/main/java/com/example/sprint1/view/DiningActivity.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1/view/DiningActivity.java
@@ -2,6 +2,7 @@ package com.example.sprint1.view;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.ImageView;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -106,17 +108,15 @@ public class DiningActivity extends AppCompatActivity {
         tabs.addTab(tabs.newTab().setText("Date"));
         tabs.addTab(tabs.newTab().setText("Name"));
 
-        tabs.selectTab(tabs.getTabAt(0));
-
         tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                tab.view.setBackgroundColor(getResources().getColor(R.color.golden));
+
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-                tab.view.setBackgroundColor(getResources().getColor(R.color.snackbar_background));
+
             }
 
             @Override
