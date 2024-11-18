2f658cc2f096978e1cfb1766a783a2279c56e13b
Bimikel <mzuo30@gatech.edu>
2024-11-17 20:59:34 -0500

    Removed errors

--- Project/Sprint1-main/app/src/main/java/com/example/sprint1/view/TravelFormEntryDialog.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1/view/TravelFormEntryDialog.java
@@ -66,8 +66,8 @@ public class TravelFormEntryDialog extends DialogFragment {
         // Observes changes in the Live Data
         observers();
 
-        // Live check to see if inputs are edited after errors
-        textWatchers();
+//        // Live check to see if inputs are edited after errors
+//        textWatchers();
 
         return binding.getRoot();
     }
@@ -267,20 +267,20 @@ public class TravelFormEntryDialog extends DialogFragment {
 
     }
 
-    private void textWatchers() {
-        // Checks if duration text field is edited after error is shown
-        durationText.addTextChangedListener(new android.text.TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-
-            public void afterTextChanged(android.text.Editable s) {
-                // Sets error to null if field is edited
-                durationText.setError(null);
-            }
-        });
-    }
+//    private void textWatchers() {
+//        // Checks if duration text field is edited after error is shown
+//        durationText.addTextChangedListener(new android.text.TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//            }
+//
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//            }
+//
+//            public void afterTextChanged(android.text.Editable s) {
+//                // Sets error to null if field is edited
+//                durationText.setError(null);
+//            }
+//        });
+//    }
 }
