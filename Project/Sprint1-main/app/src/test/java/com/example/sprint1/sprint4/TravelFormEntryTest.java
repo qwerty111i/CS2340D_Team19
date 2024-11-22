8193021d3c100c75b2ef50d41a348a5acfb62b7d
leila-kazemzadeh <leila.kazemzadeh302@gmail.com>
2024-11-21 23:46:11 -0500

    junit tests added

---
+++ Project/Sprint1-main/app/src/test/java/com/example/sprint1/sprint4/TravelFormEntryTest.java
@@ -0,0 +1,34 @@
+package com.example.sprint1.sprint4;
+
+import com.example.sprint1.model.ReservationDetails;
+import com.example.sprint1.model.TFEUser;
+import com.example.sprint1.model.TravelFormEntry;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TravelFormEntryTest {
+    // Leila Kazemzadeh
+    @Test
+    public void TravelFormEntryCheck1() {
+        TravelFormEntry entry1 = new TravelFormEntry("05/21/23", "05/24/23", "Atlanta", "a Hotel", "a cafe, a restaurant", "3");
+        Assert.assertEquals(entry1.getStartDate(), "05/21/23");
+        Assert.assertEquals(entry1.getEndDate(), "05/24/23");
+        Assert.assertEquals(entry1.getDestination(), "Atlanta");
+        Assert.assertEquals(entry1.getAccommodation(), "a Hotel");
+        Assert.assertEquals(entry1.getDining(), "a cafe, a restaurant");
+        Assert.assertEquals(entry1.getRating(), "3");
+    }
+//Leila Kazemzadeh
+    @Test
+    public void TravelFormEntryCheck2() {
+        TravelFormEntry entry1 = new TravelFormEntry("10/21/20", "10/30/20", "Paris", "somePlace", "eatery", "10");
+        Assert.assertEquals(entry1.getStartDate(), "10/21/20");
+        Assert.assertEquals(entry1.getEndDate(), "10/30/20");
+        Assert.assertEquals(entry1.getDestination(), "Paris");
+        Assert.assertEquals(entry1.getAccommodation(), "somePlace");
+        Assert.assertEquals(entry1.getDining(), "eatery");
+        Assert.assertEquals(entry1.getRating(), "10");
+    }
+
+}
